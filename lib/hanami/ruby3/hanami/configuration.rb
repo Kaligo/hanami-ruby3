e06325fc361da4d56e83b2014f424c70acb2c12d
Sebastian Wilgosz <sebastian.wilgosz@kaligo.com>
2023-02-08 12:53:54 +0100

    Add suppport for custom logger definition
    If custom logger instance is set in settings, or logger will return
    empty hash as a last argument, which will cause sequel to raise error
    when trying to call the logging methods on empty hash.

--- lib/hanami/ruby3/hanami/configuration.rb
+++ lib/hanami/ruby3/hanami/configuration.rb
@@ -4,8 +4,14 @@ module Hanami
     def logger(*args, **kwargs)
       if args.empty? && kwargs.empty?
         settings.fetch(:logger, nil)
+      # NOTE: Custom logger definition support, like:
+      # environment :test do
+      #   logger Logger.new(level: :fatal)
       elsif kwargs.empty?
         settings[:logger] = [*args]
+      # NOTE: Hash-based logger definition support, like:
+      # environment :test do
+      #   logger level: :fatal
       else
         settings[:logger] = [*args, kwargs]
       end
